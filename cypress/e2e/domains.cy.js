909943f955d1d4b95d40a1a1b95247d27cd3ca85
Batuhan Güleç <batuhan.gulec170@gmail.com>
2024-08-27 11:50:38 +0300

    add json

--- cypress/e2e/domains.cy.js
+++ cypress/e2e/domains.cy.js
@@ -1,6 +1,5 @@
 /// <reference types="cypress" />
-const domainsString = Cypress.env('DOMAINS'); // Get the domains string from environment variables
-const domains = JSON.parse(domainsString); // Parse the string into an array
+const domains = Cypress.env('DOMAINS'); // Get the domains array from environment variables
 
 describe('Domain Expiry Date Check', () => {
   domains.forEach((domain) => {
@@ -8,25 +7,21 @@ describe('Domain Expiry Date Check', () => {
       cy.visit(`https://who.is/whois/${domain}`);
 
       // Target the "Expires On" field
-      cy.get('.queryResponseBodyRow').contains('Expires On').parent().find('.queryResponseBodyValue')
-        .invoke('text')
-        .then((expiryDateText) => {
-          const expiryDate = new Date(expiryDateText.trim());
-          const currentDate = new Date();
-          const fifteenDaysFromNow = new Date();
-          fifteenDaysFromNow.setDate(currentDate.getDate() + 15); // 15 days from now
+      cy.get('.queryResponseBodyRow').contains('Expires On').parent().find('.queryResponseBodyValue').then(($expiryDateElement) => {
+        const expiryDateText = $expiryDateElement.text().trim();
+        const expiryDate = new Date(expiryDateText);
 
-          if (expiryDate < fifteenDaysFromNow) {
-            cy.log(`${domain} expires within 15 days!`); // Log to Cypress
-            assert.fail(`${domain} expires within 15 days!`); // Fail the test
-          } else {
-            cy.log(`${domain} is safe, expiry date is more than 15 days away.`); // Log to Cypress
-          }
-        })
-        .catch((error) => {
-          cy.log(`Error processing ${domain}: ${error.message}`); // Log error if any
-          assert.fail(`Failed to check expiry date for ${domain}`); // Fail the test if there's an error
-        });
+        const currentDate = new Date();
+        const sixtyDaysFromNow = new Date();
+        sixtyDaysFromNow.setDate(currentDate.getDate() + 15); // 15 gün sonra
+
+        if (expiryDate < sixtyDaysFromNow) {
+          console.log(`${domain} 15 gün içinde sona eriyor!`); // Tarayıcı konsoluna log yazdır
+          throw new Error(`${domain} 15 gün içinde sona eriyor!`);
+        } else {
+          console.log(`${domain} güvenli, son kullanma tarihi 15 günden fazla bir süre uzakta.`); // Tarayıcı konsoluna log yazdır
+        }
+      });
     });
   });
 });
